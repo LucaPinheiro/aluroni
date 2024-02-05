d6e95dc3df6b302ec2fbc259c2b7df66df1aa86b
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-05 17:19:39 -0300

    iniciando react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Inicio from "./pages/Inicio";
+import Router from "./routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
   </React.StrictMode>
 );
