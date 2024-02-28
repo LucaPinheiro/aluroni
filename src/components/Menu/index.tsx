3108350f2b5a530e082a9e54b39c4b9db02c2ad9
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-28 13:32:29 -0300

    criando navbar

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,9 +2,30 @@ import logo from "../../assets/logo.svg";
 import styles from "./Menu.module.scss";
 
 export default function Menu() {
+  const rotas = [
+    {
+      label: "Inicio",
+      to: "/",
+    },
+    {
+      label: "Cardápio",
+      to: "/cardapio",
+    },
+    {
+      label: "Sobre",
+      to: "/sobre",
+    },
+  ];
   return (
     <nav className={styles.menu}>
       <img src={logo} alt="logo do aluroni" />
+      <ul className={styles.menu__link}>
+        {rotas.map((rota, index) => (
+          <li key={index} className={styles.menu__link}>
+            <a href={rota.to}>{rota.label}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
